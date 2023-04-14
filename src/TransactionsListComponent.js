2a18d2ce880e27185c0788df03fdaf3a1721f931
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-04-13 22:33:01 -0600

    Improve transactions list component

--- src/TransactionsListComponent.js
+++ src/TransactionsListComponent.js
@@ -1,10 +1,12 @@
 import Transaction from './TransactionComponent'
 export default function TransactionList({ transactions }) {
-    const txs = transactions.map(tx => <li><Transaction tx={tx} /> </li>)
+    const txMoreCount = transactions.length - 10
+    const txs = transactions.slice(0, 10).map(tx => <li><Transaction tx={tx} /> </li>)
     return (
         <>
             <h2> Block transactions ({transactions.length}): </h2>
             <ol>{txs}</ol>
+            <span>and {txMoreCount} more</span>
         </>
     )
 }
