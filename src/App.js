fc39f4da11e3b1909ab8e71abe991fac2d29ee46
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-04-19 21:19:56 -0600

    Make final app layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TransactionList from './TransactionsListComponent';
 import Account from './Account';
 import { useState } from 'react';
 import Transaction from './TransactionComponent';
+import { Grid } from 'semantic-ui-react';
 
 function App() {
   const [blockTag, setBlockTag] = useState('latest')
@@ -14,16 +15,29 @@ function App() {
   document.title = "Ultralight block explorer"
 
   return (
-    <div>
-      <Block
-        blockTag={blockTag}
-        setBlockTag={setBlockTag}
-        block={block}
-        setBlock={setBlock}
-        setTransactions={setTransactions} />
-      <TransactionList transactions={transactions} setTx={setTx} />
-      <div><Transaction tx={tx} /></div>
-    </div>
+    <Grid columns={2} padded style={{ height: '100vh' }}>
+      <Grid.Row style={{ height: '50%' }}>
+        <Grid.Column width={50}>
+          <Block
+            blockTag={blockTag}
+            setBlockTag={setBlockTag}
+            block={block}
+            setBlock={setBlock}
+            setTransactions={setTransactions} />
+        </Grid.Column>
+        <Grid.Column width={50}>
+          <TransactionList transactions={transactions} setTx={setTx} />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row style={{ height: '50%' }}>
+        <Grid.Column width={50}>
+          <Transaction tx={tx} />
+        </Grid.Column>
+        <Grid.Column width={50}>
+          <Account />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 }
 
