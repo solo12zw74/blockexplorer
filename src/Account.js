81088d548c4c314ec2168181502671c1f949f2ee
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-04-20 21:52:48 -0600

    IMplement account balance

--- src/Account.js
+++ src/Account.js
@@ -1,6 +1,22 @@
-import { Label, Segment } from "semantic-ui-react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Label, Segment, Table } from "semantic-ui-react";
+import alchemy from "./AlchemySDK";
+import { Utils } from "alchemy-sdk";
+
 
 export default function Account({ acc }) {
+
+    const [balance, setBalance] = useState(0)
+
+    useEffect(() => {
+        async function getBlock() {
+            const b = await alchemy.core.getBalance(acc)
+            setBalance(b)
+        }
+
+        getBlock();
+    }, [acc, setBalance])
 
     if (!acc) {
         return <Segment>
@@ -14,5 +30,13 @@ export default function Account({ acc }) {
             Account
         </Label>
         <Label>{acc}</Label>
+        <Table celled>
+            <Table.Body>
+                <Table.Row>
+                    <Table.Cell>Amount</Table.Cell>
+                    <Table.Cell>{Utils.formatEther(balance)} ETH</Table.Cell>
+                </Table.Row>
+            </Table.Body>
+        </Table>
     </Segment>
 }
